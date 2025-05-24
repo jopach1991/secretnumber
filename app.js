fc2c2e4e705770667970171caaefee473cc8e35b
Jordano <jo.pacheco@live.com>
2025-05-24 10:50:29 -0300

    alterar numero para 100

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Lista vazia para adicionar elementos
 let listaDeNumerosSorteados = [];
 
-let numeroLimite = 10;
+let numeroLimite = 100;
 
 //Variavel reservando espaço para o valor geradoo pela função
 let numeroSecreto = gerarNumeroAleatorio();
@@ -21,7 +21,7 @@ function exibirTextoNaTela(tag, texto){
 //Função criada para evitar repetição do código
 function mensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto!');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 //chamada para a função
 mensagemInicial()
